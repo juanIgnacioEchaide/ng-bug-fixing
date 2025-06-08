ae5355261a42f74b58279ce2664a5ecac8e98bcd
juaniechaide <juaniechaide@gmail.com>
2025-06-08 16:41:28 -0300

    initial solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemDisplayComponent } from "./item-display/components/item-display.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemDisplayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
